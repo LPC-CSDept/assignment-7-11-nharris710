77c9a5264c2509ae125c97811ce02f7fb5d12cb0
nharris710 <noaharris248@gmail.com>
2023-05-23 21:32:35 -0700

    grouped graph for scores

--- main.py
+++ main.py
@@ -13,6 +13,13 @@ names = ['Bill', 'Mary']
 # ******************************
 # Make your code
 # ******************************
-
+ax.bar(x-width*1.5,Math,width)
+ax.bar(x-width*1.5,English,width)
+ax.bar(x-width*1.5,Physics,width)
+ax.bar(x-width*1.5,Computer,width)
+ax.legend()
+ax.set_title('Grouped graph for scores')
+ax.bar_label(ax.continers[0])
+ax.set_xticks(x - width)
 
 fig.savefig('A11.png')
